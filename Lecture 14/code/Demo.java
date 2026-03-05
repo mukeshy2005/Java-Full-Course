65f709b04d8fe692fb03d86747183762dc41964b
Aditya Tandon <40351467+adityatandon15@users.noreply.github.com>
2026-03-05 22:02:39 +0530

    Add files via upload

---
+++ Lecture 14/code/Demo.java
@@ -0,0 +1,22 @@
+// Call by Value
+
+public class Demo {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+
+        System.out.println(x + " , " + y);
+
+        addTen(x, y);
+        
+        System.out.println(x + " , " + y);
+
+    }
+
+    static void addTen(int x, int y) {
+        x = x + 10;
+        y = y + 10;
+    }
+}
+
+
